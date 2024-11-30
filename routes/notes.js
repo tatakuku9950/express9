11ec2ddfc5da5dbf1f068be8e98da2dea96000d1
tatakuku9950 <sr20det31spr@hotmail.com>
2024-11-30 10:07:21 +0900

    add api9

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const{ MongoClient} = require("mongodb");
+const{MongoClient} = require("mongodb");
 const uri= "mongodb+srv://sr20det31spr:r32r33r34@test.kkz50.mongodb.net/?retryWrites=true&w=majority&appName=test";
 const client= new MongoClient(uri);
 router.get('/', async(req, res) =>{
